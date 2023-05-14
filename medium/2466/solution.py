b4a20ea54bc4cf5cfd92feabc28c324f691c0ddb
Aref Ghodamai <aref.ghodamai@divar.ir>
2023-05-14 15:13:56 +0330

    add problem 2466

---
+++ medium/2466/solution.py
@@ -0,0 +1,23 @@
+from base_test import BaseSolutionTest
+
+
+class Solution:
+
+    def __init__(self):
+        self.main = self.countGoodStrings
+
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        kMod = 1_000_000_007
+        ans = 0
+        dp = {0: 1}
+        for i in range(1, high + 1):
+            if i >= zero:
+                dp[i] = (dp.get(i, 0) + dp.get(i - zero, 0)) % kMod
+            if i >= one:
+                dp[i] = (dp.get(i, 0) + dp.get(i - one, 0)) % kMod
+            if i >= low:
+                ans = (ans + dp[i]) % kMod
+        return ans
+
+
+BaseSolutionTest(Solution, )

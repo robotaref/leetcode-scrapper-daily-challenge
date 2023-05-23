cdae70eb1491c6bb7b050e3faa36c67cfccbf553
Aref Ghodamai <aref.ghodamai@gmail.com>
2023-05-23 23:32:36 +0330

    add 349

---
+++ medium/347/solution.py
@@ -0,0 +1,16 @@
+import collections
+from typing import List
+
+from base_test import BaseSolutionTest
+
+
+class Solution:
+
+    def __init__(self):
+        self.main = self.topKFrequent
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [i for i, j in collections.Counter(nums).most_common(k)]
+
+
+BaseSolutionTest(Solution, )
---
+++ medium/399/solution.py
@@ -0,0 +1,45 @@
+import collections
+from typing import List, Set
+
+from base_test import BaseSolutionTest
+
+
+class Solution:
+
+    def __init__(self):
+        self.main = self.calcEquation
+
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[
+        float]:
+        ans = []
+        graph = collections.defaultdict(dict)
+
+        for (A, B), value in zip(equations, values):
+            graph[A][B] = value
+            graph[B][A] = 1 / value
+
+        def divide(A: str, C: str, seen: Set[str]) -> float:
+            if A == C:
+                return 1.0
+
+            seen.add(A)
+
+            for B, value in graph[A].items():
+                if B in seen:
+                    continue
+                res = divide(B, C, seen)
+                if res > 0:
+                    return value * res
+
+            return -1.0
+
+        for A, C in queries:
+            if A not in graph and C not in graph:
+                ans.append(-1.0)
+            else:
+                ans.append(divide(A, C, set()))
+
+        return ans
+
+
+BaseSolutionTest(Solution,)

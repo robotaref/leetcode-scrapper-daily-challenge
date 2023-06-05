14173dfbde7ff20964726762c347b3fc640525c1
Aref Ghodamai <aref.ghodamai@divar.ir>
2023-06-05 09:41:47 +0330

    add 1376

---
+++ medium/547/solution.py
@@ -0,0 +1,32 @@
+from collections import defaultdict, deque
+from typing import List
+
+from testing.solution_test import BaseSolutionTest
+
+
+class Solution:
+    def __init__(self):
+        self.main = self.findCircleNum
+        self.isConnected = [[]]
+        self.colored = []
+        self.n = 0
+
+    def color(self, i, d):
+        self.colored[i] = d
+        for j, c in enumerate(self.isConnected[i]):
+            if i != j and c == 1 and not self.colored[j]:
+                self.color(j, d)
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        self.n = len(isConnected)
+        self.isConnected = isConnected
+        self.colored = [False] * self.n
+        c = 0
+        for i in range(self.n):
+            if not self.colored[i]:
+                c += 1
+                self.color(i, c)
+        return c
+
+
+BaseSolutionTest(Solution, )
